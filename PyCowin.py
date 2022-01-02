fc3d12a2a712cc4043436bb4200e01906e5adb77
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2022-01-02 19:16:18 +0530

    Update PyCowin.py

--- PyCowin.py
+++ PyCowin.py
@@ -6,7 +6,7 @@ import base64
 from fake_useragent import UserAgent
 
 ua = UserAgent()
-header = {'User-Agent': str(ua.chrome)}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 state_response = requests.get(f"https://cdn-api.co-vin.in/api/v2/admin/location/states",headers=header)
 states = state_response.json()
 states_dict = {}
